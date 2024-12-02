c31332ccfb55987b4cf79f59723c289cc8bc2d08
TharushaK01 <tharushakavinda01@gmail.com>
2024-12-02 10:11:00 +0530

    Update2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BabyInPictureComponent } from './baby-in-picture/baby-in-picture.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'baby-in-picture', component: BabyInPictureComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/baby-in-picture/baby-in-picture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baby-in-picture',
+  templateUrl: './baby-in-picture.component.html',
+  styleUrl: './baby-in-picture.component.css'
+})
+export class BabyInPictureComponent {
+
+}
