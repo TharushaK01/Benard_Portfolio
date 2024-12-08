05d384169228263020a6feb996db0b95b77f3d3e
TharushaK01 <tharushakavinda01@gmail.com>
2024-12-08 06:47:31 +0530

    data and time

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrl: './calender.component.css'
 })
-export class CalenderComponent {
+export class CalenderComponent implements OnInit{
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+      
+  }
+  home(){
+    this.router.navigate(['/home']);
+  }
 
 }
