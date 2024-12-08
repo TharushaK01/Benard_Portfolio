bdae87bcf50c994a04e87e7da8c8796268b73d4a
TharushaK01 <tharushakavinda01@gmail.com>
2024-12-08 05:56:18 +0530

    Calender work!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BabyinpitctureComponent } from './babyinpitcture/babyinpitcture.component';
+import { CalenderComponent } from './calender/calender.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BabyinpitctureComponent
+    BabyinpitctureComponent,
+    CalenderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/babyinpitcture/babyinpitcture.component.ts
+++ src/app/babyinpitcture/babyinpitcture.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-babyinpitcture',
   templateUrl: './babyinpitcture.component.html',
   styleUrl: './babyinpitcture.component.css'
 })
-export class BabyinpitctureComponent {
-
+export class BabyinpitctureComponent implements OnInit{
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+      
+  }
+  calender(){
+    this.router.navigate(['/calender']);
+  }
 }
